213cbdcf3078d60e00c0c4dbb5257e8c6e80572a
KRISH MISHRA <ashamishra122098@gmail.com>
2026-02-27 13:23:03 +0530

    Update app to run on configurable port

--- app.py
+++ app.py
@@ -46,5 +46,8 @@ def upload():
 def files(filename):
     return app.send_from_directory(UPLOAD_FOLDER, filename)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
